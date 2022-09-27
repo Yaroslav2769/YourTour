0159c5afb51926e9a0edaeecb9efc8c144182bf1
Yaroslav2769 <belyaev_yaroslav02@mail.ru>
2022-09-28 00:14:21 +0300

    update

--- common.js
+++ common.js
@@ -1,6 +1,6 @@
 var element = document.getElementById('phone');
 var maskOptions = {
-  mask: '+7(0 0 0) 0 0 0-0 0-0 0',
+  mask: '+7(0 0 0) 0 0 0 - 0 0 - 0 0',
  
 };
 var mask = IMask(element, maskOptions);
